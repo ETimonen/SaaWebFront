04947f14e09d229c0fdcbfc2ac04d756017721f1
Eero Timonen <eero.timonen@student.careeria.fi>
2025-12-10 18:19:09 +0200

    1.0 toimii

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import DataList from './DataList';
 import axios from 'axios';
@@ -6,22 +5,8 @@ import axios from 'axios';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <div className="DataList">
-      <h1>Data List Sorted by Date</h1>
+      <h1>Espoo</h1>
       <DataList /> {}
       </div>
     </div>
@@ -37,9 +22,9 @@ axios.get('https://saa-api.onrender.com/saaapi/ennusteet/')
     if (error.response) {
       console.log(error.response.data);
     } else if (error.request) {
-      console.log(error.request);
+      console.log('Error:', error.request);
     } else {
-      console.log('Error', error.message);
+      console.log('Error:', error.message);
     }
   });
 
--- src/DataList.js
+++ src/DataList.js
@@ -1,6 +1,34 @@
-// src/DataList.js
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import axios from 'axios';
+
+// Funktio, joka hakee arvon tulevalta päivämäärältä
+const getValueFromFutureDate = (currentDate, data, dataType, daysToAdd) => {
+  const targetDate = new Date(currentDate);
+  targetDate.setDate(targetDate.getDate() + daysToAdd); // Lisätään päivämäärään daysToAdd (esim. 3 tai 10 päivää)
+
+  // Etsi data-objektista päivämäärä, joka vastaa targetDate
+  const futureData = data.find(item => {
+    const itemDate = new Date(item.pvm);
+    return itemDate.toLocaleDateString() === targetDate.toLocaleDateString();
+  });
+
+  if (futureData) {
+    switch (dataType) {
+      case 'temp':  // Lämpötila
+        return futureData.lampotila_nyt !== null ? futureData.lampotila_nyt : '-';
+      case 'cloud':  // Pilvisyys
+        const parsedCloud = parseFloat(futureData.pilvisyys_nyt)
+        return parsedCloud.toFixed(1) !== null ? parseFloat(futureData.pilvisyys_nyt).toFixed(1) : '-';
+      case 'rain':  // Sademäärä
+        return futureData.sade_nyt !== null ? futureData.sade_nyt : '-';
+      default:
+        return '-';
+    }
+  }
+
+  return '-';
+};
+
 
 function DataList() {
   const [data, setData] = useState([]);
@@ -10,7 +38,7 @@ function DataList() {
     // Hae data API:sta
     axios.get('https://saa-api.onrender.com/saaapi/ennusteet/')
       .then(response => {
-        setData(response.data); // Oletetaan, että data on vastausobjektissa
+        setData(response.data);
         setLoading(false);
       })
       .catch(error => {
@@ -19,70 +47,107 @@ function DataList() {
       });
   }, []);
 
-  // Lajittele data päivämäärän ('pvm') mukaan
-  const sortedData = data.sort((a, b) => {
-    const dateA = new Date(a.pvm);
-    const dateB = new Date(b.pvm);
-    return dateA - dateB; // Pienemmät päivämäärät tulevat ensin
-  });
-
-  // Funktio vertailuun ennusteen ja kolme päivää myöhemmin toteutuneen lämpötilan välillä
-  const compareTemperatures = (forecastDate, forecastTemp) => {
-    // Etsi kolme päivää ennusteen jälkeen toteutunut lämpötila
-    const forecastDateObj = new Date(forecastDate);
-    const targetDate = new Date(forecastDateObj);
-    targetDate.setDate(targetDate.getDate() + 3); // Siirretään päivämäärää kolme päivää eteenpäin
-
-    // Etsi tietokannasta merkintä, joka vastaa kolme päivää myöhempää päivämäärää
-    const correspondingDate = sortedData.find(item => {
-      const compareDateObj = new Date(item.pvm);
-      return compareDateObj.getTime() === targetDate.getTime(); // Vertaa aikaleimoja tarkasti
-    });
-    
-    // Jos löydetään vastaava toteutunut lämpötila, lasketaan ero
-    if (correspondingDate) {
-      const temperatureDifference = correspondingDate.lampotila_nyt - forecastTemp;
-      return temperatureDifference.toFixed(2); // Pyöristetään kahteen desimaaliin
-    }
-    return null; // Jos ei löydy vastaavaa tietoa
-  };
+  const sortedData = useMemo(() => {
+    return data.sort((a, b) => new Date(a.pvm) - new Date(b.pvm));
+  }, [data]);
 
   return (
-    <div>
+    <div className="data-list-container">
       {loading ? (
-        <p>Loading...</p>
+        <p>Ladataan...</p>
       ) : (
         <div>
-          <h2>Lämpötilan vertailu: Ennuste vs Toteutunut (3 päivää myöhemmin)</h2>
+          <h2>Sään ennusteet ja toteutuneet arvot</h2>
           <table>
             <thead>
+              {/* Yläotsikot */}
               <tr>
-                <th>Päivämäärä (ennuste)</th>
-                <th>Ennuste (3pv) (°C)</th>
-                <th>Toteutunut lämpötila (3pv myöhemmin) (°C)</th>
-                <th>Ero toteutuneeseen lämpötilaan (°C)</th>
+                <th rowSpan="3">Päivämäärä (ennuste)</th>
+                <th colSpan="9" className="group3">3 päivän ennuste</th>
+                <th colSpan="9" className="group10">10 päivän ennuste</th>
+              </tr>
+
+              {/* Ryhmäotsikot */}
+              <tr>
+                <th colSpan="3" className="group3">Lämpötila (°C)</th>
+                <th colSpan="3" className="group3">Sademäärä (mm)</th>
+                <th colSpan="3" className="group3">Pilvisyys (%)</th>
+
+                <th colSpan="3" className="group10">Lämpötila (°C)</th>
+                <th colSpan="3" className="group10">Sademäärä (mm)</th>
+                <th colSpan="3" className="group10">Pilvisyys (%)</th>
+              </tr>
+
+              {/* Sarakkeiden nimet */}
+              <tr>
+                {/* 3pv */}
+                <th className="group3">Ennuste</th>
+                <th className="group3">Toteutunut</th>
+                <th className="group3">Ero</th>
+                <th className="group3">Ennuste</th>
+                <th className="group3">Toteutunut</th>
+                <th className="group3">Ero</th>
+                <th className="group3">Ennuste</th>
+                <th className="group3">Toteutunut</th>
+                <th className="group3">Ero</th>
+
+                {/* 10pv */}
+                <th className="group10">Ennuste</th>
+                <th className="group10">Toteutunut</th>
+                <th className="group10">Ero</th>
+                <th className="group10">Ennuste</th>
+                <th className="group10">Toteutunut</th>
+                <th className="group10">Ero</th>
+                <th className="group10">Ennuste</th>
+                <th className="group10">Toteutunut</th>
+                <th className="group10">Ero</th>
               </tr>
             </thead>
+
             <tbody>
               {sortedData.map((item, index) => {
-                const temperatureDifference = compareTemperatures(item.pvm, item.lampotila_3pv);
+                // 3 päivän ennusteet
+                const tempDiff3 = getValueFromFutureDate(item.pvm, sortedData, 'temp', 3) !== '-' ? getValueFromFutureDate(item.pvm, sortedData, 'temp', 3) - item.lampotila_nyt : '-';
+                const rainDiff3 = getValueFromFutureDate(item.pvm, sortedData, 'rain', 3) !== '-' ? getValueFromFutureDate(item.pvm, sortedData, 'rain', 3) - item.sade_nyt : '-';
+                const cloudDiff3 = (item.pilvisyys_3pv !== null && item.pilvisyys_nyt !== null) ? (parseFloat(item.pilvisyys_3pv) - parseFloat(item.pilvisyys_nyt)) : '-';
+                // 10 päivän ennusteet
+                const tempDiff10 = getValueFromFutureDate(item.pvm, sortedData, 'temp', 10) !== '-' ? getValueFromFutureDate(item.pvm, sortedData, 'temp', 10) - item.lampotila_nyt : '-';
+                const rainDiff10 = getValueFromFutureDate(item.pvm, sortedData, 'rain', 10) !== '-' ? getValueFromFutureDate(item.pvm, sortedData, 'rain', 10) - item.sade_nyt : '-';
+                const cloudDiff10 = getValueFromFutureDate(item.pvm, sortedData, 'cloud', 10) !== '-' ? (parseFloat(getValueFromFutureDate(item.pvm, sortedData, 'cloud', 10)) - parseFloat(item.pilvisyys_nyt)) : '-';
+
                 return (
                   <tr key={index}>
                     <td>{new Date(item.pvm).toLocaleDateString()}</td>
-                    <td>{item.lampotila_3pv} °C</td>
-                    {/* Etsi kolme päivää ennusteen jälkeen toteutunut lämpötila ja lasketaan ero */}
-                    <td>
-                      {temperatureDifference !== null
-                        ? sortedData.find(d => new Date(d.pvm).getTime() === new Date(item.pvm).getTime() + 3*24*60*60*1000)?.lampotila_nyt
-                        : 'Ei tietoa'}
-                    </td>
-                    {/* Ero toteutuneeseen lämpötilaan */}
-                    <td>
-                      {temperatureDifference !== null
-                        ? temperatureDifference
-                        : 'Ei tietoa'}
-                      °C
-                    </td>
+
+                    {/* Lämpötila 3pv */}
+                    <td className="group3">{item.lampotila_3pv !== null ? item.lampotila_3pv + "°C" : '-'}</td>
+                    <td className="group3">{getValueFromFutureDate(item.pvm, sortedData, 'temp', 3) !== '-' ? getValueFromFutureDate(item.pvm, sortedData, 'temp', 3) + "°C" : '-'}</td>
+                    <td className="group3">{tempDiff3 !== '-' ? tempDiff3.toFixed(2) : '-'}</td>
+
+                    {/* Sademäärä 3pv */}
+                    <td className="group3">{item.sade_3pv !== null ? item.sade_3pv + " mm" : '-'}</td>
+                    <td className="group3">{getValueFromFutureDate(item.pvm, sortedData, 'rain', 3) !== '-' ? getValueFromFutureDate(item.pvm, sortedData, 'rain', 3) + " mm" : '-'}</td>
+                    <td className="group3">{rainDiff3 !== '-' ? rainDiff3.toFixed(2) : '-'}</td>
+
+                    {/* Pilvisyys 3pv */}
+                    <td className="group3">{item.pilvisyys_3pv !== null ? item.pilvisyys_3pv + " %" : '-'}</td>
+                    <td className="group3">{getValueFromFutureDate(item.pvm, sortedData, 'cloud', 3) !== '-' ? getValueFromFutureDate(item.pvm, sortedData, 'cloud', 3) + " %" : '-'}</td>
+                    <td className="group3">{cloudDiff3 !== '-' ? cloudDiff3.toFixed(1) : '-'}</td>
+
+                    {/* Lämpötila 10pv */}
+                    <td className="group10">{item.lampotila_10pv !== null ? item.lampotila_10pv + "°C" : '-'}</td>
+                    <td className="group10">{getValueFromFutureDate(item.pvm, sortedData, 'temp', 10) !== '-' ? getValueFromFutureDate(item.pvm, sortedData, 'temp', 10) + "°C" : '-'}</td>
+                    <td className="group10">{tempDiff10 !== '-' ? tempDiff10.toFixed(2) : '-'}</td>
+
+                    {/* Sademäärä 10pv */}
+                    <td className="group10">{item.sade_10pv !== null ? item.sade_10pv + " mm" : '-'}</td>
+                    <td className="group10">{getValueFromFutureDate(item.pvm, sortedData, 'rain', 10) !== '-' ? getValueFromFutureDate(item.pvm, sortedData, 'rain', 10) + " mm" : '-'}</td>
+                    <td className="group10">{rainDiff10 !== '-' ? rainDiff10.toFixed(2) : '-'}</td>
+
+                    {/* Pilvisyys 10pv */}
+                    <td className="group10">{item.pilvisyys_10pv !== null ? item.pilvisyys_10pv + " %" : '-'}</td>
+                    <td className="group10">{getValueFromFutureDate(item.pvm, sortedData, 'cloud', 10) !== '-' ? getValueFromFutureDate(item.pvm, sortedData, 'cloud', 10) + " %" : '-'}</td>
+                    <td className="group10">{cloudDiff10 !== '-' ? cloudDiff10.toFixed(2) : '-'}</td>
                   </tr>
                 );
               })}
